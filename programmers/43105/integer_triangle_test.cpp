e9be70e57484e14c5a606e42f2aa2c67e3f2d670
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-05-20 21:11:09 +0900

    refactor code

--- programmers/43105/integer_triangle_test.cpp
+++ programmers/43105/integer_triangle_test.cpp
@@ -9,8 +9,7 @@ GTEST_TEST(IntegerTriangleSum, Successful)
         {3, 8},
         {8, 1, 0},
         {2, 7, 4, 4},
-        {4, 5, 2, 6, 5}
-    };
+        {4, 5, 2, 6, 5}};
     constexpr auto expected = 30;
     const auto actual = IntegerTriangleSum(triangle);
     EXPECT_EQ(expected, actual);
--- programmers/64063/allocation_room_number_test.cpp
+++ programmers/64063/allocation_room_number_test.cpp
@@ -5,8 +5,8 @@
 GTEST_TEST(allocation_room_number, Successful)
 {
     auto k = 10;
-    const auto room_number = std::vector<long long>{ 1, 3, 4, 1, 3, 1 };
-    const auto expected = std::vector<long long>{ 1, 3, 4, 2, 5, 6 };
+    const auto room_number = std::vector<long long>{1, 3, 4, 1, 3, 1};
+    const auto expected = std::vector<long long>{1, 3, 4, 2, 5, 6};
 
     const auto actual = AllocationRoomNumber(room_number);
     EXPECT_EQ(expected, actual);
