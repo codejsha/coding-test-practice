89d42be04998ec20380b4772e49352deac3858fc
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-05-27 01:57:52 +0900

    add PROGRAMMERS-12911

---
+++ programmers/problem12911/NextBigNumber.java
@@ -0,0 +1,18 @@
+package problem12911;
+
+public class NextBigNumber {
+    public static void main(String[] args) {
+        int n = 78;
+        var sol = new NextBigNumber();
+        System.out.println(sol.solution(n));
+        System.out.println(Integer.toBinaryString(sol.solution(n)));
+    }
+
+    private int solution(int n) {
+        var bits = Integer.bitCount(n);
+        do {
+            n++;
+        } while (bits != Integer.bitCount(n));
+        return n;
+    }
+}
